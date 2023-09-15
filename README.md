ac6d14c89d7926a785a8f6c80c1591e9bc3d2255
probablyalright <ricards877@gmail.com>
2023-09-15 20:17:40 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Why?
+This project is made for a github and automatization course in university.
+
+### Api that is being used:
+https://www.n2yo.com/api/#visualpasses
+
+### To be continued . . .
---
+++ naked.py
@@ -0,0 +1,29 @@
+import requests
+
+# Replace 'abcd-qwer-asdf' with your personal API key
+API_KEY = 'abcd-qwer-asdf'
+# Base URL for N2YO API
+BASE_URL = 'https://api.n2yo.com/rest/v1/satellite/'
+# Norad id for satellite. 25544 = ISS
+SATELLITE_ID = 25544
+# Observer data: decimal degrees - latitude, longitude; meters - elevation.
+LAT = 57.54161
+LON = 25.42826
+ALT = 0
+# In seconds - length of time while satellite is visible in the sky
+VISIBILITY = 60
+# In days - how far into the future to predict ISS passovers, MAX = 10.
+DAYS = 10
+
+# Prepares appended string to URL to get visual passes
+def get_visual_passes():
+    url = f"{BASE_URL}visualpasses/{SATELLITE_ID}/{LAT}/{LON}/{ALT}/{DAYS}/{VISIBILITY}&apiKey={API_KEY}"
+    response = requests.get(url)
+    return response.json()
+
+if __name__ == "__main__":
+ 
+    # Get visual passes for ISS. Print out received information.
+    visual_passes_response = get_visual_passes()
+    print("\nVisual Passes Response:")
+    print(visual_passes_response)
