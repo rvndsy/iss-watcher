65df4ec281527730b95bc579929fe9ecd48f96b6
probablyalright <112282114+probablyalright@users.noreply.github.com>
2023-10-14 23:42:18 +0300

    Renamed logging file

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from configparser import ConfigParser
 
 # Loading logging configuration
-with open('./log_worker.yaml.dev', 'r') as stream:
+with open('./log_main.yaml', 'r') as stream:
     config = yaml.safe_load(stream)
 logging.config.dictConfig(config)
 logger = logging.getLogger('root')
